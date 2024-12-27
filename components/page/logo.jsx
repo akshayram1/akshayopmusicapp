8a4a7821c22c62f880f52cc6865540eeaf3bd875
akshayram1 <akshaychame2@gmail.com>
2024-12-27 14:22:21 +0530

    changes

--- components/page/logo.jsx
+++ components/page/logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
     return (
         <Link href="/" className="select-none">
             <div>
-                <h1 className="text-2xl font-bold">Move<span className="opacity-50">On</span></h1>
+                <h1 className="text-2xl font-bold">Akshay<span className="opacity-50">Op</span></h1>
             </div>
         </Link>
     )
