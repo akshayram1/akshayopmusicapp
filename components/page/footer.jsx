fd679d9606acef7781c7bbe9a099fd91f5f79b15
akshayram1 <akshaychame2@gmail.com>
2024-12-27 14:23:02 +0530

    gg

--- components/page/footer.jsx
+++ components/page/footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="py-5 backdrop-blur-3xl mt-8 px-6 md:px-20 lg:px-32">
             <div>
-                <h1 className="text-xl font-bold">Music<span className="opacity-50">hub</span></h1>
+                <h1 className="text-xl font-bold">Akshay<span className="opacity-50">Op</span></h1>
                 <p className="text-xs text-muted-foreground">Made with Nextjs by <a className="underline hover:text-primary" >Akshay Chame</a> hosted on vercel.</p>
             </div>
             <p className="text-muted-foreground text-sm mt-2 max-w-lg">We don't own any of the songs and images it belongs to its respective owners, built for educational purposes.</p>
